bd43f62c5d45bffa5b4d977a3d0be7a85f402977
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-10 17:22:45 +0200

    pages and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,20 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import { Route, Navigate, Routes } from "react-router-dom"
+
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Chat from "./pages/Chat"
 
 function App() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  const action = (
-    <React.Fragment>
-      <Button color="secondary" size="small" onClick={handleClose}>
-        UNDO
-      </Button>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleClose}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </React.Fragment>
-  );
-
+  
   return (
     <>
-     <h1>Empty</h1>
-     <Button onClick={handleClick}>Open simple snackbar</Button>
-<Snackbar
-  open={open}
-  autoHideDuration={6000}
-  onClose={handleClose}
-  message="Note archived"
-  action={action}
-/>
-
-<Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <Fab color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
-      <Fab color="secondary" aria-label="edit">
-        <EditIcon />
-      </Fab>
-      <Fab variant="extended">
-        <NavigationIcon sx={{ mr: 1 }} />
-        Navigate
-      </Fab>
-      <Fab disabled aria-label="like">
-        <FavoriteIcon />
-      </Fab>
-    </Box>
-    </>
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to={"/ "} />} />
+    </Routes>
+  </> 
   )
 }
 
