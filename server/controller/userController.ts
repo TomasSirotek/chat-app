93611e625a67b7e2be811c76bf2c0c3215dcf3b9
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-11 10:56:14 +0200

    serve fixed authorization with jwt stored in cookie and protected routes

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -1,17 +1,50 @@
 import { User, PostUserDto } from "../model/user";
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { autoInjectable } from "tsyringe";
 import UserService from "../service/userService";
 import JWT from "../helpers/jwt";
+import { Role } from "../helpers/role";
+import {
+	ReasonPhrases,
+	StatusCodes,
+	getReasonPhrase,
+	getStatusCode,
+} from 'http-status-codes';
 
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 
+const ACCESS_TOKEN = 'access_token';
+
+const authorization = (req: any, res: Response, next: NextFunction) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+    });
+  }
+
+  try {
+    const data = JWT.verifyToken(token);
+    req.userId = data.id;
+    req.userRole = data.role;
+    return next();
+  } catch {
+    return res.status(StatusCodes.FORBIDDEN)
+    .send({
+      error: getReasonPhrase(StatusCodes.FORBIDDEN)
+    });
+  }
+};
+
 @autoInjectable()
 export default class UserController {
   userService: UserService;
+
 
   constructor(userService: UserService) {
     this.userService = userService;
@@ -22,18 +55,18 @@ export default class UserController {
 
     // validation of the user input
     if (!username || !email || !password)
-      return res.status(400).json({ msg: "Please fill all the fields" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please fill all the fields" });
     if (!validator.isEmail(email))
-      return res.status(400).json({ msg: "Please enter a valid email" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please enter a valid email" });
     if (!validator.isStrongPassword(password))
-      return res.status(400).json({ msg: "Please enter a strong password" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please enter a strong password" });
 
     // checking if the user already exists
     const existingUser: User | undefined =
       await this.userService.getUserByEmailAsync(email);
 
     if (existingUser)
-      return res.status(400).json({ msg: "User already exists" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "User already exists" });
 
     const user: PostUserDto = {
       username,
@@ -47,58 +80,62 @@ export default class UserController {
       user
     );
 
-    if (!newUser) return res.status(500).json({ msg: "Failed to create user" });
+    if (!newUser) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Failed to create user" });
 
-    const token = JWT.createToken(newUser.id);
+    const token = JWT.createToken(newUser.id, Role.ADMIN);
 
-    // For now returning use inside the json response however it has to be changed back later on to the token only ! Propabllyyyy
-    // Same here with the cookie
     return res
-      .cookie("access_token", token, {
+      .cookie(ACCESS_TOKEN, token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
       })
-      .status(200)
+      .status(StatusCodes.OK)
       .json({
-        message: "Registered successfully 😊 👌",
+        message: "Registered successfully",
         _id: newUser.id,
         name: newUser.username,
       });
   }
 
-  async authenticate(req: Request, res: Response) {
+  async login(req: Request, res: Response) {
     const { email, password } = req.body;
 
     console.log(req.body);
 
     if (!email || !password)
-      return res.status(400).json({ msg: "Please fill all the fields" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please fill all the fields" });
 
     const existingUser: User | undefined =
       await this.userService.getUserByEmailAsync(email);
 
     // checking if the user exists
     if (!existingUser)
-      return res.status(400).json({ msg: "User does not exist" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "User does not exist" });
 
     const isValidPws = await bcrypt.compare(password, existingUser.password);
     if (!isValidPws)
-      return res.status(400).json({ msg: "Invalid credentials" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Invalid credentials" });
 
-    const token = JWT.createToken(existingUser.id);
+    const token = JWT.createToken(existingUser.id, Role.ADMIN);
 
     return res
-      .cookie("access_token", token, {
+      .cookie(ACCESS_TOKEN, token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
       })
-      .status(200)
+      .status(StatusCodes.OK)
       .json({
-        message: "Loggen in successfully 😊 👌",
+        message: "Loggen in successfully",
         _id: existingUser.id,
         name: existingUser.username,
       });
+  }
 
+  async logout(req: Request, res: Response) {
+    return res
+      .clearCookie(ACCESS_TOKEN)
+      .status(200)
+      .json({ message: "Successfully logged out." });
   }
 
   async getUserById(req: Request, res: Response) {
@@ -108,32 +145,33 @@ export default class UserController {
       userId
     );
 
-    if (!user) return res.status(404).json({ msg: "User not found" });
+    if (!user) return res.status(StatusCodes.NOT_FOUND).json({ msg: "User not found" });
 
-    res.status(200).json(user);
+    res.status(StatusCodes.OK).json(user);
   }
 
   async getAllUsers(req: Request, res: Response) {
     const users: User[] | undefined = await this.userService.getAllUsersAsync();
 
-    if (!users) return res.status(404).json({ msg: "No users found" });
+    if (!users) return res.status(StatusCodes.NOT_FOUND).json({ msg: "No users found" });
 
-    res.status(200).json(users);
+    res.status(StatusCodes.OK).json(users);
   }
 
   routes() {
     router.post("/register", (req: Request, res: Response) =>
       this.registerUser(req, res)
     );
-
-    router.post("/authenticate", (req: Request, res: Response) =>
-      this.authenticate(req, res)
+    router.post("/login", (req: Request, res: Response) =>
+      this.login(req, res)
     );
-
-    router.get("/:id", (req: Request, res: Response) =>
+    router.get("/logout", authorization, (req: Request, res: Response) =>
+      this.logout(req, res)
+    );
+    router.get("/:id",authorization, (req: Request, res: Response) =>
       this.getUserById(req, res)
     );
-    router.get("/", (req: Request, res: Response) =>
+    router.get("/", authorization,(req: Request, res: Response) =>
       this.getAllUsers(req, res)
     );
     return router;
--- server/helpers/jwt.ts
+++ server/helpers/jwt.ts
@@ -1,10 +1,17 @@
+import { Role } from "./role";
+
 const jwt = require("jsonwebtoken");
 
 export default class JWT {
 
-    static createToken = (_id: number) => {
+    static createToken = (_id: number,_role: Role) => {
         const jwtkey = process.env.JWT_SECRET;
       
-        return jwt.sign({ id: _id }, jwtkey, { expiresIn: '3d' });
+        return jwt.sign({ id: _id, role: _role }, jwtkey, { expiresIn: '3d' });
       };
+
+    static verifyToken = (token: string) => {
+        const jwtkey = process.env.JWT_SECRET;
+        return jwt.verify(token, jwtkey);
+      }  
 }
