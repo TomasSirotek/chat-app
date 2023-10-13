23e441882134a8f13920525431a1b041b5f3b6b0
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-13 07:43:14 +0200

    messages model

---
+++ server/model/message.ts
@@ -0,0 +1,24 @@
+import { Member } from "./member";
+
+export interface Message {
+    chatId : number,
+    senderId: number,
+    body: string,
+    created_at: Date,
+  }
+
+export class MessageModel implements Message{
+    chatId : number;
+    senderId: number;
+    body: string;
+    created_at: Date;
+
+    constructor(chatId : number, senderId: number, body: string, created_at: Date) {
+        this.chatId = chatId;
+        this.senderId = senderId;
+        this.body = body;
+        this.created_at = created_at;
+    }
+
+    
+}
